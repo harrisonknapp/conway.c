fceda807e9e405181d887262de5b52ff1356ab00
Harrison Knapp <121597472+harrisonknapp@users.noreply.github.com>
2023-02-06 22:46:32 -0600

    Added structure and most functions
    I need to write the whole iteration function. It's not been tested past that, and I may try iteration with a placeholder now. I am getting some weird error in repeatPrompt()'s print statement but we'll see. The hardest parts should be board generation and iterating and if I've got the first down I'm halfway there.

---
+++ conway.c
@@ -0,0 +1,69 @@
+#include <stdio.h> //Duh
+#include <time.h> //To randomize seed
+#include <stdlib.h> //To randomize
+
+int gameBoard[10][10];
+int nextgameBoard[10][10];
+int iterationsCompleted  = 0;
+
+void iterate() {}
+
+int repeatPrompt(int iterationNum) {
+   char repeatResponse[50];
+   /*
+   The array above should not have to take as much memory as it does. However, it's what I've got for now. I don't want the user to have to repeat their input if it's not simply a character  (as it should be simple to quit) but for now I'll have to work with a string as that's what I know that will provide some sort of input freedom.
+   Limitations suck, and this one is on my knowledge lol
+   */
+   printf("Iteration %d: ", iterationNum);
+   scanf("%s", repeatResponse);
+
+   if (strcmp("q", repeatResponse) == 0) return 1;
+   else return 0;
+}
+
+int generateBoard(void) {
+
+   int startNum = 0;
+   printf("Begin with how many squares? ");
+
+   scanf("%d", &startNum);
+   while (startNum < 1) {
+      printf("That is not a valid input. Must be positive integer: ");
+      scanf("%d", &startNum);
+      //FIXME: Need a fix here so other types of input don't hang up
+   }
+   
+   for (int i = 0; i < startNum; ++i) { //Clear the board
+      for (int j = 0; j < startNum; ++j) {
+         gameBoard[i][j] = 0;
+      }
+   }
+
+   for (int i = 0; i < startNum;) {
+      int iVal = rand() % 10, jVal = rand() % 10;
+      switch (gameBoard[iVal][jVal]) {
+         case 0:
+            gameBoard[iVal][jVal] = 1;
+            ++i;
+            break;
+         case 1:     //This should make it so if a square selected is already generated alive, another square will be generated alive. There should be startNum unique squares that begin alive.
+            break; 
+      }
+   }
+   return 0;
+}
+
+int main() {
+
+   srand(time(NULL));
+
+   printf("Once the simulation begins, insert anything other than \"q\" to continue.\n");
+   generateBoard();
+   
+   int cont = 0;
+   while (!cont) {
+      iterate();
+      cont = repeatPrompt(iterationsCompleted);
+   }
+   return 0;
+}
